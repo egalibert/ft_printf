31c8798433364cad734176de99fc0eb1a2ed9b4f
Elliot Galibert <elliotgalibert@Elliot-MacBook-Pro.local>
2022-09-20 00:14:00 +0300

    fixed norm and negative numbers problem

--- src/ft_tools2.c
+++ src/ft_tools2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tools2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elliotgalibert <elliotgalibert@student.    +#+  +:+       +#+        */
+/*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:09:39 by elliotgalib       #+#    #+#             */
-/*   Updated: 2022/09/19 21:40:38 by elliotgalib      ###   ########.fr       */
+/*   Updated: 2022/09/20 00:12:24 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_add_hash(t_flags *flags, char *str, unsigned long long num)
 int	ft_power(int power, int base)
 {
 	int	i;
-	int number;
+	int	number;
 
 	number = 1;
 	i = 0;
@@ -38,4 +38,39 @@ int	ft_power(int power, int base)
 		i++;
 	}
 	return (number);
+}
+
+long double	ft_manage_f_mods(va_list *args, t_flags *flags)
+{
+	if (flags->mods == 5)
+		return ((long double)va_arg(*args, long double));
+	else
+		return ((double)va_arg(*args, double));
+}
+
+long double	ft_rounder(long double num, t_flags *flags)
+{
+	long double	temp;
+	int			is_neg;
+
+	is_neg = 0;
+	temp = num;
+	if (num < 0)
+	{
+		num *= -1;
+		is_neg = 1;
+	}
+	temp = num * ft_power(flags->dot, 10);
+	if (temp - (long long)temp == 0.5)
+	{
+		if ((long long)temp % 2 == 0)
+			num = temp / ft_power(flags->dot, 10);
+		else
+			num = (temp + 0.5) / ft_power(flags->dot, 10);
+	}
+	else
+		num = (temp + 0.5) / ft_power(flags->dot, 10);
+	if (is_neg == 1)
+		num *= -1;
+	return (num);
 }
--- src/ft_treat_float.c
+++ src/ft_treat_float.c
@@ -3,43 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   ft_treat_float.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elliotgalibert <elliotgalibert@student.    +#+  +:+       +#+        */
+/*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 19:26:03 by elliotgalib       #+#    #+#             */
-/*   Updated: 2022/09/19 22:49:33 by elliotgalib      ###   ########.fr       */
+/*   Updated: 2022/09/20 00:11:10 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include <stdio.h>
 
+char	*ft_manage_f_wid(t_flags *flags, char *str)
+{
+	int		len;
+	char	*temp_str;
+	int		a;
+
+	a = 0;
+	len = flags->width - ft_strlen(str);
+	temp_str = ft_strnew(len);
+	while (len--)
+	{
+		if (flags->zero == 1 && flags->dot == -1 && flags->minus == 0)
+			temp_str[a++] = '0';
+		else
+			temp_str[a++] = ' ';
+	}
+	if (flags->minus == 1)
+		str = ft_strjoin_f1(str, temp_str);
+	else
+		str = ft_strjoin_f2(temp_str, str);
+	free(temp_str);
+	return (str);
+}
+
 char	*ft_manage_f_str(t_flags *flags, char *str, long double number)
 {
-	char	*temp;
+	int	len;
 
-	temp = NULL;
 	if (number < 0)
 	{
-		temp = ft_strjoin_f2("-",str);
-		free(str);
-		str = temp;
+		str = ft_strjoin_f2("-", str);
 	}
 	else if (number >= 0 && (flags->plus == 1 || flags->space == 1))
 	{
 		if (flags->plus == 1)
-			temp = ft_strjoin_f2("+",str);
+			str = ft_strjoin_f2("+", str);
 		else if (flags->space == 1)
-			temp = ft_strjoin_f2(" ", str);
-		free(str);
-		str = temp;
+			str = ft_strjoin_f2(" ", str);
 	}
+	len = ft_strlen(str);
+	if (flags->width >= len)
+		str = ft_manage_f_wid(flags, str);
 	return (str);
 }
 
 char	*ft_add_float_part(long double f_num, char *str, int precision)
 {
 	long double	num;
-	int	i;
+	int			i;
 
 	num = f_num - (long long)f_num;
 	str = ft_strnew(precision + 1);
@@ -58,7 +80,7 @@ char	*ft_add_float_part(long double f_num, char *str, int precision)
 
 char	*ft_ftoa(long double num, char *str, int precision)
 {
-	char			*float_part;
+	char	*float_part;
 
 	float_part = NULL;
 	str = ft_itoa_signed((long long)num);
@@ -68,51 +90,14 @@ char	*ft_ftoa(long double num, char *str, int precision)
 		float_part = ft_strdup("");
 	str = ft_strjoin_f1(str, float_part);
 	free(float_part);
-	
-	/*if (1.0 - remainder < 0 + remainder
-		|| (1.0 - remainder == 0 + remainder
-			&& ((str)[ft_strlen((str)) - 1] + 1 - '0') % 2 == 0))
-		round_up(str, ft_strlen(str) - 1);*/
 	return (str);
-}
-
-long double	ft_manage_f_mods(va_list *args, t_flags *flags)
-{
-	if (flags->mods == 5)
-		return ((long double)va_arg(*args, long double));
-	else
-		return ((double)va_arg(*args, double));
-
-}
-
-long double	ft_rounder(long double num, t_flags *flags)
-{
-	long double	temp;
-
-	temp = num;
-	if (num < 0)
-		num *= -1;
-	temp = num * ft_power(flags->dot, 10);
-	if (temp - (long long)temp == 0.5)
-	{
-		// if number is 0
-		if ((long long)temp % 2 == 0)
-			num = temp / ft_power(flags->dot, 10);
-		else
-			// temp = temp + 0.5
-			num = (temp + 0.5) / ft_power(flags->dot, 10);
-	}
-	else
-		num = (temp + 0.5) / ft_power(flags->dot, 10);
-	//printf("value of num is |%Lf|\n", num);
-	return (num);
 }
 
 int	ft_treat_float(va_list *args, t_flags *flags)
 {
-	char					*str;
-	long double				number;
-	int						char_count;
+	char		*str;
+	long double	number;
+	int			char_count;
 
 	str = NULL;
 	char_count = 0;
@@ -121,7 +106,7 @@ int	ft_treat_float(va_list *args, t_flags *flags)
 		flags->dot = 6;
 	number = ft_rounder(number, flags);
 	str = ft_ftoa(number, str, flags->dot);
-	//str = ft_manage_f_str(flags, str, number);
+	str = ft_manage_f_str(flags, str, number);
 	ft_putstr(str);
 	char_count = ft_strlen(str);
 	free(str);
