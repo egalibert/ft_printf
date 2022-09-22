43ba88b65383f7cfe44c20447162903d10739de3
Elliot Galibert <elliotgalibert@Elliot-MacBook-Pro.local>
2022-09-22 13:06:09 +0300

    .h file fixed

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:07:28 by elliotgalib       #+#    #+#             */
-/*   Updated: 2022/09/20 00:11:01 by egaliber         ###   ########.fr       */
+/*   Updated: 2022/09/22 13:03:35 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ typedef struct s_flags
 	int				mods;
 	int				over;
 	int				min_over;
+	int				star;
 	unsigned int	base;
 }					t_flags;
 
@@ -48,7 +49,7 @@ int					ft_treat_uns_int(va_list *args, t_flags *flags);
 int					ft_flag_dot(const char *save, int start, t_flags *flags, \
 								va_list args);
 int					ft_putchar2(int c);
-int					ft_power(int power, int base);
+long double			ft_power(int power, long double base);
 int					ft_treat_pointer(va_list *args, t_flags *flags);
 int					ft_check_mods(int c);
 int					ft_strlen2(const char *str);
@@ -58,10 +59,12 @@ long long int		ft_manage_mods(va_list *args, t_flags *flags);
 unsigned long long	ft_manage_u_mods(va_list *args, t_flags *flags);
 long double			ft_manage_f_mods(va_list *args, t_flags *flags);
 long double			ft_rounder(long double num, t_flags *flags);
+long double			ft_rounding(int i, long double num);
 
 char				*ft_manage_int_sign(t_flags *flags, char *str, \
 										long long int num);
 char				*ft_manage_int_zero(t_flags *flags, char *str);
+char				*ft_hex_hash_mod(t_flags *flags, char *str, unsigned long long num, int len2);
 char				*ft_strjoin_f2(char *s1, char *s2);
 char				*ft_strjoin_f1(char *s1, char *s2);
 char				*ft_itoa_signed(long long int n);
@@ -70,14 +73,17 @@ char				*ft_str_tolower(char *str);
 char				ft_char_sign(t_flags *flags, char sign);
 char				*ft_add_hash(t_flags *flags, char *str, \
 								unsigned long long num);
+char				*ft_add_minus(char *str, t_flags *flags, int is_neg);
 
 void				ft_flag_parse(char *str, t_flags *flags);
 void				ft_init_flags(t_flags *flags);
+void				ft_star(va_list *args, t_flags *flags);
 
-t_flags				ft_flag_minus(t_flags flags);
+/*t_flags				ft_flag_minus(t_flags flags);
 t_flags				ft_flag_width(va_list *args, t_flags flags);
 t_flags				ft_flag_digit(char c, t_flags flags);
 t_flags				ft_flag_plus(t_flags flags);
 t_flags				ft_flag_hash(t_flags flags);
+*/
 
 #endif
